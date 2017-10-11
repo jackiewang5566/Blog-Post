a39e58c577e698d0e99a3ae861bf3c4c157726e6
Di Wang <jackiewang5566@hotmail.com>
2017-10-11 15:17:23 -0400

    add react-router for postsNew component

--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import { Provider } from 'react-redux';
 import { createStore, applyMiddleware } from 'redux';
-import { BrowserRouter, Route } from 'react-router-dom';
+import { BrowserRouter, Route, Switch } from 'react-router-dom';
 import promise from 'redux-promise';
 
 import reducers from './reducers';
@@ -15,8 +15,18 @@ ReactDOM.render(
   <Provider store={createStoreWithMiddleware(reducers)}>
     <BrowserRouter>
       <div>
-        <Route path="/" component={PostsIndex} />
-        <Route path="/posts/new" component={PostsNew} />
+        {/* 
+          Difference between previous version router and react-router version 4:
+          the switch component takes in a different collection of routes,
+          in practice we used to nest a list of routes in it. The switch 
+          component will look inside of it and then it will decide to only 
+          render the first route that matches the current url, so in other words,
+          you need to put your most specific routes on the top of the list. 
+        */}
+        <Switch>
+          <Route path="/posts/new" component={PostsNew} />
+          <Route path="/" component={PostsIndex} />
+        </Switch>
       </div>
     </BrowserRouter>
   </Provider>
