679b9a7517522a37e113e1c788ee2ba2f8a42979
Di Wang <jackiewang5566@hotmail.com>
2017-10-12 10:44:56 -0400

    install redux-form and generalized field component in form for postsNew component

--- src/actions/index.js
+++ src/actions/index.js
@@ -2,7 +2,7 @@ import axios from 'axios';
 
 export const FETCH_POSTS = 'fetch_posts';
 const ROOT_URL = 'http://reduxblog.herokuapp.com/api';
-const API_KEY = '?key=diwang9022'
+const API_KEY = '?key=dw9022'
 
 export function fetchPosts() {
     const request = axios.get(`${ROOT_URL}/posts${API_KEY}`);
