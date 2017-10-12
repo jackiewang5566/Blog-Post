e5e9cfbf75e8d3b83f574eff29c39d33f097ee18
Di Wang <jackiewang5566@hotmail.com>
2017-10-12 15:30:12 -0400

    add fetch_post actionCreator

--- src/index.js
+++ src/index.js
@@ -8,6 +8,7 @@ import promise from 'redux-promise';
 import reducers from './reducers';
 import PostsIndex from './components/posts_index';
 import PostsNew from './components/posts_new';
+import PostsShow from './components/posts_show'
 
 const createStoreWithMiddleware = applyMiddleware(promise)(createStore);
 
@@ -24,8 +25,9 @@ ReactDOM.render(
           you need to put your most specific routes on the top of the list. 
         */}
         <Switch>
-          <Route path="/posts/new" component={PostsNew} />
-          <Route path="/" component={PostsIndex} />
+          <Route path="/posts/new" component={ PostsNew } />
+          <Route path="/posts/:id" component={ PostsShow } />
+          <Route path="/" component={ PostsIndex } />
         </Switch>
       </div>
     </BrowserRouter>
