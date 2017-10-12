998159507b3b30a9ddff906732cf88a14c1a9cd8
Di Wang <jackiewang5566@hotmail.com>
2017-10-12 14:49:56 -0400

    add onSubmit to post a post msg to backend API

--- src/components/posts_new.js
+++ src/components/posts_new.js
@@ -1,6 +1,8 @@
 import React, { Component } from 'react';
 import { Field, reduxForm } from 'redux-form';
-import { Link } from 'react-router-dom'; 
+import { Link } from 'react-router-dom';
+import { connect } from 'react-redux';
+import { createPost } from '../actions';
 
 class PostsNew extends Component {
     renderField(field) {
@@ -24,7 +26,11 @@ class PostsNew extends Component {
     }
 
     onSubmit(values) {
+        // this === component
         console.log(values);
+        this.props.createPost(values, () => {
+            this.props.history.push('/');
+        });
     }
 
     // The Field component which we are using to specify an input inside of 
@@ -89,4 +95,6 @@ function validate(values) {
 export default reduxForm({
     validate,
     form: 'PostsNewForm'
-})(PostsNew);
+})(
+    connect(null, { createPost })(PostsNew)
+);
