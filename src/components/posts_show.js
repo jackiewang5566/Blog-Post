57885493c7858a2c2a3747a84f483d74a3b4854c
Di Wang <jackiewang5566@hotmail.com>
2017-10-13 01:38:02 -0400

    add delete function on postsShow page

--- src/components/posts_show.js
+++ src/components/posts_show.js
@@ -2,6 +2,7 @@ import React, { Component } from 'react';
 import { connect } from 'react-redux';
 import { Link } from 'react-router-dom';
 import { fetchPost } from '../actions';
+import { deletePost } from '../actions';
 
 class PostsShow extends Component {
     componentDidMount() {
@@ -15,6 +16,13 @@ class PostsShow extends Component {
         this.props.posts[this.props.match.params.id];
     }
 
+    onDeleteClick() {
+        const { id } = this.props.match.params;
+        this.props.deletePost(id, () => {
+            this.props.history.push('/');
+        });
+    }
+
     render() {
         // posts[this.props.match.params.id]; // the post we want to show
         const { post } = this.props;
@@ -26,6 +34,10 @@ class PostsShow extends Component {
         return (
             <div>
                 <Link to="/">Back to Index</Link>
+                <button className="btn btn-danger pull-xs-right"
+                        onClick={this.onDeleteClick.bind(this)}>
+                        Delete Post
+                </button>
                 <h3>{ post.title }</h3>
                 <h6>Categories: { post.categories }</h6>
                 <p>{ post.content }</p>
@@ -40,4 +52,4 @@ function mapStateToProps({ posts }, ownProps) {
     return { post: posts[ownProps.match.params.id] };
 }
 
-export default connect(mapStateToProps, { fetchPost })(PostsShow);
+export default connect(mapStateToProps, { fetchPost, deletePost })(PostsShow);
